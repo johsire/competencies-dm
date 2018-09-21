3f6890c2fbd3d1d498558c59b21104569e36fd59
Josiah Maina <josiah5115@gmail.com>
2018-09-21 12:14:07 -0600

    arrays-3-Q3

--- arrays-3.js
+++ arrays-3.js
@@ -12,10 +12,9 @@
   
   //Code here
   function sunset(array) {
-    for (var i = 0; i < array.length; i++)
-      array[0] = 'blue';
-      return array;
+    return array;
   };
+  sunset(sunsetColors);
 
 //////////////////PROBLEM 2////////////////////
 
@@ -26,6 +25,10 @@
   var sunriseColors = ["yellow", "orange", "red", "pink", "purple", "blue"]
   
   //Code here
+  function sunrise(array) {
+      return array;
+  };
+  sunrise(sunriseColors);
 
 
 //////////////////PROBLEM 3////////////////////
@@ -45,11 +48,11 @@
 //                 the copy and the original; a copy made by value will only 
 //                 change the copy's values`
 
-// var refVsVal = `Passing by reference points to the same object in memory;
-//                 pass by value creates a new object in memory with the same 
-//                 values. A change made to a reference copy will change both 
-//                 the copy and the original; a copy made by value will only 
-//                 change the copy's values`
+var refVsVal = `Passing by reference points to the same object in memory;
+                pass by value creates a new object in memory with the same 
+                values. A change made to a reference copy will change both 
+                the copy and the original; a copy made by value will only 
+                change the copy's values`
 
 // var refVsVal = `Passing by reference creates a new array in memory that 
 //                 points to the original array in memory; pass by value creates 
